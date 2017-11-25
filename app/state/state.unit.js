fcdd26fb74309992c08236479cf42a1d89f70c8a
Chris <chrisregner.com@gmail.com>
2017-11-26 00:33:03 +0800

    Initial dist subtree commit

--- app/state/state.unit.js
+++
@@ -1,3 +0,0 @@
-import { test } from 'mocha'
-
-test.skip('state | it should automatically delegate the getters')
--- openConsoles.bat
+++
@@ -1,4 +0,0 @@
-start cmd /k "yarn test --verbose"
-start cmd /k ""
-start cmd /k "yarn start"
-exit
